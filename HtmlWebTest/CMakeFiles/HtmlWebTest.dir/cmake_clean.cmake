71cd4c1493d7ea3afe2299a1839a8413cf2f0017
RewriteWASD <90543914+RewriteWASD@users.noreply.github.com>
2022-06-18 19:11:12 +0800

    Add files via upload

---
+++ HtmlWebTest/CMakeFiles/HtmlWebTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/HtmlWebTest.dir/main.cpp.o"
+  "CMakeFiles/HtmlWebTest.dir/ServerSock.cpp.o"
+  "CMakeFiles/HtmlWebTest.dir/MysqlConnect.cpp.o"
+  "HtmlWebTest.pdb"
+  "HtmlWebTest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/HtmlWebTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
